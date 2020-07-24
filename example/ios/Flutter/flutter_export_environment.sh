2e8665e42eabada583609e3642f960465acf03a7
ToCHe <rerom2536@gmail.com>
2020-07-24 12:55:12 +0700

    update lib camera android

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/admin/Desktop/Work/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/admin/Desktop/Work/TCC/flutter_qr_reader/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/admin/Desktop/Work/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
